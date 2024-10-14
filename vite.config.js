a22522ba231b9b7bb607b862ab34e18ba5d10b00
peiyang <114836719+Peiyangg@users.noreply.github.com>
2024-10-14 10:58:17 +0200

    update config

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,9 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 export default defineConfig({
   base: "/github-repo-test/",
   plugins: [svelte()],
-})
+  resolve: {
+    dedupe: ['d3']
+  },
+  optimizeDeps: {
+    include: ['d3']
+});
